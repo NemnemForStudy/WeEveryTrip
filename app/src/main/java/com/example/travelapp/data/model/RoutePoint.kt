6812c9a0709ed7814c28f9a5292b08f110bc54bd
NemnemForStudy <dlackdtjq123@naver.com>
2026-01-07 20:18:38 +0900

    2026-01-07 코딩
    RoutePoint 주석처리(중복 선언 되었음)

--- app/src/main/java/com/example/travelapp/data/model/RoutePoint.kt
+++ app/src/main/java/com/example/travelapp/data/model/RoutePoint.kt
@@ -1,16 +1,16 @@
-package com.example.travelapp.data.model
-
-data class RoutePoint(
-    val latitude: Double,
-    val longitude: Double
-)
-
-// 서버로 보낼 때: { "locations": [...]
-data class RouteRequest(
-    val locations: List<RoutePoint>
-)
-
-// 서버에서 받을 때
-data class RouteResponse(
-    val route: List<RoutePoint>
-)
+//package com.example.travelapp.data.model
+//
+//data class RoutePoint(
+//    val latitude: Double,
+//    val longitude: Double
+//)
+//
+//// 서버로 보낼 때: { "locations": [...]
+//data class RouteRequest(
+//    val locations: List<RoutePoint>
+//)
+//
+//// 서버에서 받을 때
+//data class RouteResponse(
+//    val route: List<RoutePoint>
+//)
