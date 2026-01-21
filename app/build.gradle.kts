c85ea32fbcedfee7f92541b71f412546ea520248
NemnemForStudy <dlackdtjq123@naver.com>
2026-01-21 20:16:53 +0900

    2026-01-21 구글 로그인 에러나서 수정(버전 변경)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.nemnem.travelapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
