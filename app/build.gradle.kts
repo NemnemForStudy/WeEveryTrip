ce5e69b8d3ec3d1050970195fcd125505386baeb
NemnemForStudy <dlackdtjq123@naver.com>
2026-02-08 12:02:31 +0900

    2026-02-07 어플 배포 전 에러 수정(사진 올리기)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.nemnem.travelapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
